83e06b00ef7f9139a9ed35c0fc2e7b6ea5026ffc
henry-herrmann <henryherrmann.tv@gmail.com>
2022-05-06 15:44:59 +0200

    commit

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.static("../build"));
+
+app.get('/', function (req, res) {
+    res.sendFile('../build/index.html');
+});
+
+app.listen(8090, () =>{
+    console.log("[INFO] Portfolio webserver started.")
+})
